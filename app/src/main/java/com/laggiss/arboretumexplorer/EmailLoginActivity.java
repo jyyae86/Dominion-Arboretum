1c061b1a0bc98a2014fc7f727774785c809743d5
Iain Jiang <jyyae86@gmail.com>
2017-05-10 11:46:31 -0400

    Added firebase and created user auth

---
+++ app/src/main/java/com/laggiss/arboretumexplorer/EmailLoginActivity.java
@@ -0,0 +1,95 @@
+package com.laggiss.arboretumexplorer;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class EmailLoginActivity extends AppCompatActivity {
+
+    //UI attributes
+    EditText email;
+    EditText password;
+    Button signUp;
+    Button login;
+    ProgressDialog progressDialog;
+
+    //Firebase
+    FirebaseAuth mAuth;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_login);
+
+        mAuth = FirebaseAuth.getInstance();
+
+//        if(mAuth != null){
+//            Log.e("reached auth","tst");
+//            finish();
+//            startActivity((new Intent(getApplicationContext(), MainActivity.class)));
+//        }
+
+        email = (EditText)findViewById(R.id.editTextEmail);
+        password = (EditText)findViewById(R.id.editTextPassword);
+        signUp = (Button)findViewById(R.id.buttonSignUp);
+        login = (Button)findViewById(R.id.buttonLogin);
+        progressDialog = new ProgressDialog(this);
+    }
+
+    protected void loginWithEmail(View v){
+        String eml = email.getText().toString();
+        String pswd = password.getText().toString();
+
+        if(TextUtils.isEmpty(eml)){
+            Toast.makeText(this,"Email cannot be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pswd)){
+            Toast.makeText(this,"Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //start progress dialog
+        progressDialog.setMessage("loading, please wait");
+        progressDialog.show();
+
+        mAuth.signInWithEmailAndPassword(eml, pswd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()){
+                            finish();
+                            startActivity((new Intent(getApplicationContext(), MainActivity.class)));
+                        }else{
+
+                        }
+                    }
+                });
+    }
+
+    protected void createSignUpActivity(View v){
+        //redirects to signup
+        finish();
+        startActivity(new Intent(this,EmailSignUpActivity.class));
+    }
+
+    protected void createMainActivityWithoutLogin(View v){
+        finish();
+        startActivity(new Intent(this,MainActivity.class));
+    }
+
+}
---
+++ app/src/main/java/com/laggiss/arboretumexplorer/EmailSignUpActivity.java
@@ -0,0 +1,85 @@
+package com.laggiss.arboretumexplorer;
+
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class EmailSignUpActivity extends AppCompatActivity {
+    Button signIn;
+    EditText email;
+    EditText password;
+    ProgressDialog progressDialog;
+
+    //instance of Firebase Auth
+    private FirebaseAuth mAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_email_sign_up);
+
+        //initialize Firebase auth
+        mAuth = FirebaseAuth.getInstance();
+
+        //initialize UI attributes
+        signIn = (Button)findViewById(R.id.buttonSignUp);
+        email = (EditText)findViewById(R.id.editTextEmail);
+        password = (EditText)findViewById(R.id.editTextPassword);
+        progressDialog = new ProgressDialog(this);
+
+    }
+
+    protected void signUpWithEmail(View v){
+        String eml = email.getText().toString();
+        String pswd = password.getText().toString();
+
+        if(TextUtils.isEmpty(eml)){
+            Toast.makeText(this,"Email cannot be empty",Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if(TextUtils.isEmpty(pswd)){
+            Toast.makeText(this,"Password cannot be empty", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        //start progress dialog
+        progressDialog.setMessage("loading, please wait");
+        progressDialog.show();
+
+        mAuth.createUserWithEmailAndPassword(eml,pswd)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        //dismiss progess dialog
+                        progressDialog.dismiss();
+                        if(task.isSuccessful()) {
+                            Toast.makeText(EmailSignUpActivity.this, "Sign up successful", Toast.LENGTH_SHORT).show();
+                            finish();
+                            startActivity(new Intent(getApplicationContext(), EmailLoginActivity.class));
+                        }else{
+                            Toast.makeText(EmailSignUpActivity.this,"Sign up failed", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+    }
+
+    protected void createLoginActivity(View v){
+        finish();
+        startActivity(new Intent(this, EmailLoginActivity.class));
+    }
+}
