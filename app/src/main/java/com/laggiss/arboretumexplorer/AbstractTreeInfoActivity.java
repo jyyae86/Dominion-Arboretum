187de4d901493bf679151983f5cff8030a3b4db7
Iain Jiang <jyyae86@gmail.com>
2018-05-16 17:25:09 -0400

    Created added entries to Strings.xml and updated TreeInfo UI for new database format

--- app/src/main/java/com/laggiss/arboretumexplorer/AbstractTreeInfoActivity.java
+++ app/src/main/java/com/laggiss/arboretumexplorer/AbstractTreeInfoActivity.java
@@ -15,12 +15,12 @@ import com.google.firebase.database.DatabaseReference;
  */
 
 public abstract class AbstractTreeInfoActivity extends AppCompatActivity {
-    TextView commonName;
-    TextView sciName;
-    TextView cArea;
-    TextView lat;
-    TextView lng;
-    TextView creatorName;
+    TextView family, familiarName, genus, species, rank,
+            treeType, hybridCross, cultivar, nameStatus, authority,
+            dateIntro, accessNo, recdFrom, dateRecd, howRecd,
+            numRecd, nameRecd, commonName, nomCommun, nursery,
+            location, lat, lng, donor, collSeed, sourceAcc, revised,
+            numberNow, origins, herbSpec, idByDate, photo1, photo2, mortInfo, notes, memo;
     String id;
 
     Button editTree;
@@ -47,12 +47,42 @@ public abstract class AbstractTreeInfoActivity extends AppCompatActivity {
         deleteTree = (Button)findViewById(R.id.buttonDeleteTree);
         mergeTree = (Button)findViewById(R.id.buttonMergeTree);
 
-        commonName = (TextView) findViewById(R.id.textViewCommonName);
-        sciName = (TextView) findViewById(R.id.textViewScientificName);
-        cArea = (TextView) findViewById(R.id.textViewCrownArea);
-        lat = (TextView) findViewById(R.id.textViewLatitude);
-        lng = (TextView) findViewById(R.id.textViewLongitude);
-        creatorName = (TextView) findViewById(R.id.textViewcreatorName);
+        family = (TextView)findViewById(R.id.textViewFamily);
+        familiarName = (TextView)findViewById(R.id.textViewFamiliarName);
+        genus = (TextView)findViewById(R.id.textViewGenus);
+        species = (TextView)findViewById(R.id.textViewSpecies);
+        rank = (TextView)findViewById(R.id.textViewRank);
+        treeType = (TextView)findViewById(R.id.textViewType);
+        hybridCross = (TextView)findViewById(R.id.textViewHybridCross);
+        cultivar = (TextView)findViewById(R.id.textViewCultivar);
+        nameStatus = (TextView)findViewById(R.id.textViewNameStatus);
+        authority = (TextView)findViewById(R.id.textViewAuthority);
+        dateIntro = (TextView)findViewById(R.id.textViewDateIntro);
+        accessNo = (TextView)findViewById(R.id.textViewAccessno);
+        recdFrom = (TextView)findViewById(R.id.textViewRecdFrom);
+        dateRecd = (TextView)findViewById(R.id.textViewDateRecd);
+        howRecd = (TextView)findViewById(R.id.textViewHowRecd);
+        numRecd = (TextView)findViewById(R.id.textViewNumRecd);
+        nameRecd = (TextView)findViewById(R.id.textViewNameRecd);
+        commonName = (TextView)findViewById(R.id.editTextCommonName);
+        nomCommun = (TextView)findViewById(R.id.textViewNomCommun);
+        nursery = (TextView)findViewById(R.id.textViewNursery);
+        location = (TextView)findViewById(R.id.textViewLocation);
+        donor = (TextView)findViewById(R.id.textViewDonor);
+        collSeed = (TextView)findViewById(R.id.textViewCollseed);
+        sourceAcc = (TextView)findViewById(R.id.textViewSourceAcc);
+        revised = (TextView)findViewById(R.id.textViewRevised);
+        numberNow = (TextView)findViewById(R.id.textViewNumberNow);
+        origins = (TextView)findViewById(R.id.textViewOrigins);
+        herbSpec = (TextView)findViewById(R.id.textViewHerbSpec);
+        idByDate = (TextView)findViewById(R.id.textViewIDByDate);
+        photo1 = (TextView)findViewById(R.id.textViewPhoto1);
+        photo2 = (TextView)findViewById(R.id.textViewPhoto2);
+        mortInfo = (TextView)findViewById(R.id.textViewMortInfo);
+        notes = (TextView)findViewById(R.id.textViewNotes);
+        memo = (TextView)findViewById(R.id.textViewMemo);
+
+
         Intent intent = getIntent();
 
         id = intent.getStringExtra("id");
